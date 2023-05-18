c3d57835132812eb0c9269a8133b0ede32b52039
shlomit <shlomitshifraw@gmail.com>
2023-05-18 15:16:57 +0300

    create CRUD API for app usres

---
+++ Models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+const userModel = mongoose.model("User", UserSchema);
+
+module.exports = userModel;
